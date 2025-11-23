56759f0977436f2c5112629557d3572eff5fe24e
DharmRaj15 <dharmaraj.raut@outlook.com>
2025-11-23 11:46:56 +0530

    stopwatch code commit with html

--- src/app/stop-watch/stop-watch.component.ts
+++ src/app/stop-watch/stop-watch.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StopWatchComponent implements OnInit {
 
+  elapsedTime: number = 0;
+  inetrvalRef: any;
+  isRunning: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  startstop() {
+    this.isRunning ? this.stopStopwatch() : this.startStopwatch();
+  }
+
+  startStopwatch() {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.inetrvalRef = setInterval(() => {
+        this.elapsedTime+=0.1;
+      }, 100);
+    }
+    console.log(this.elapsedTime);
+  }
+
+  stopStopwatch() {
+    if (this.isRunning) {
+      this.isRunning = false;
+      clearInterval(this.inetrvalRef);
+    }
+    console.log(this.elapsedTime);
+  }
+
+  resetStopwatch() {
+    this.isRunning = false;
+    clearInterval(this.inetrvalRef);
+    this.elapsedTime = 0;
+    console.log(this.elapsedTime);
   }
 
 }
